7f93d71f27fd8e9aff22f3400bf6411e9b613704
Bharat Digrase <bharatdigrase276@gmail.com>
2025-03-28 14:47:16 +0530

    1

--- ThreadPriority.java
+++ ThreadPriority.java
@@ -6,36 +6,35 @@ class MyThread extends Thread {
 
 public class ThreadPriority {
     public static void main(String[] args) {
-        // Creating three threads
+        
         MyThread t1 = new MyThread();
         MyThread t2 = new MyThread();
         MyThread t3 = new MyThread();
 
-        // Setting names for the threads
+ 
         t1.setName("Thread 1");
         t2.setName("Thread 2");
         t3.setName("Thread 3");
 
-        // Displaying initial priorities
+        
         System.out.println("Initial Priorities:");
         t1.start();
         t2.start();
         t3.start();
 
-        // Changing thread priorities
         try {
-            // Sleep to ensure initial priorities are displayed before changing them
+            
             Thread.sleep(100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        // Setting new priorities
-        t1.setPriority(Thread.MIN_PRIORITY);    // 1
-        t2.setPriority(Thread.NORM_PRIORITY);   // 5
-        t3.setPriority(Thread.MAX_PRIORITY);    // 10
+       
+        t1.setPriority(Thread.MIN_PRIORITY);   
+        t2.setPriority(Thread.NORM_PRIORITY);   
+        t3.setPriority(Thread.MAX_PRIORITY);    
 
-        // Displaying new priorities
+        
         System.out.println("\nNew Priorities:");
         MyThread t4 = new MyThread();
         MyThread t5 = new MyThread();
